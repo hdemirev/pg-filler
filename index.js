1489161f28902abf69e93cf04152093114a3dd80
Hari Demirev <haridemirev@Haris-MacBook-Pro-2.local>
2021-03-02 10:07:04 -0800

    initial

---
+++ index.js
@@ -0,0 +1,47 @@
+const { Pool } = require("pg");
+
+// connects using DATABASE_URL env var
+const pool = new Pool({
+  max: 20,
+  connectionString: process.env.DATABASE_URL,
+});
+
+const init = async () => {
+  await pool.query("create table if not exists dummy_data(data) values($1)");
+  await pool.query("delete * from dummy_data");
+};
+
+const generateTasks = (numTasks) => {
+  const dummyData = {};
+  for (var j = 0; j < 10000; j++) {
+    dummyData[Math.random().toString()] = Math.random().toString();
+  }
+
+  const tasks = [];
+  for (var i = 0; i <= numTasks; i++) {
+    const task = (done) => {
+      pool
+        .query("insert into dummy_data(data) values($1)", [dummyData])
+        .then(() => {
+          done();
+        });
+    };
+    tasks.push(task);
+  }
+
+  return tasks;
+};
+
+const fillData = async (tasks) => {
+  async.parallelLimit(tasks, process.env.PARALLEL_LIMIT || 20, () => {
+    console.log("all done with inserts");
+  });
+};
+
+const main = async () => {
+  await init();
+  const tasks = generateTasks(5000);
+  fillData(tasks);
+};
+
+main();
