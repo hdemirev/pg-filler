c9a3d28b03ca09ce592d46e76b7908dfc146f9c5
Hari Demirev <haridemirev@Haris-MacBook-Pro-2.local>
2021-03-02 10:31:06 -0800

    bump json size

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ let tasksDone = 0;
 
 const generateTasks = (numTasks) => {
   const dummyData = {};
-  for (var j = 0; j < 10000; j++) {
+  for (var j = 0; j < process.env.JSON_SIZE || 50000; j++) {
     dummyData[Math.random().toString()] = Math.random().toString();
   }
 
